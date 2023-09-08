b23b1180ba69ac484dcf16c41d9f74415cce80bf
TouhinTechnical <touhidulislam1090@gmail.com>
2023-09-08 17:19:12 +0600

    project file set up

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
